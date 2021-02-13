d84f1a745f0a9fb45d693d7af5734bd8960702a5
Mohammed Elattar <mohammed.elattar@metro-markets.de>
2021-02-13 21:09:14 +0100

    scaffold frontend & backend

---
+++ src/frontend/Dockerfile
@@ -0,0 +1,4 @@
+FROM node:15-alpine
+
+# Start application
+CMD ["yarn", "serve"]
---
+++ src/frontend/src/components/HelloThere.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloThere',
+  props: {
+      msg: String
+  }
+}
+</script>
+
+<style>
+p {
+  font-family: "Arial", sans-serif;
+  font-size: 90px;
+  text-align: center;
+  font-weight: bold;
+}
+
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+}
+</style>
